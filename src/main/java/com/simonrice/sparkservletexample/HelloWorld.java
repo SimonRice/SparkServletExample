504959347dc1b07af22de4becd9366abd511120f
Simon Rice <simon@simonrice.com>
2012-11-19 21:24:28 +0000

    Initial commit

---
+++ src/main/java/com/simonrice/sparkservletexample/HelloWorld.java
@@ -0,0 +1,26 @@
+package com.simonrice.sparkservletexample;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+import spark.servlet.SparkApplication;
+
+public class HelloWorld implements SparkApplication {
+	@Override
+	public void init() {
+		Spark.get(new Route("/hello") {
+			@Override
+			public Object handle(Request request, Response response) {
+				return "Hello World!";
+			}
+		});
+
+		Spark.get(new Route("/hello/:name") {
+			@Override
+			public Object handle(Request request, Response response) {
+				return  String.format("Hello, %s!", request.params(":name"));
+			}
+		});
+	}
+}
